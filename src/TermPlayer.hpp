7b533e57cd4bfb18bf9663b8ff229ea523f998bb
Martin Leroy <martin.leroy@ecole-89.com>
2024-03-21 10:39:53 +0100

    init

---
+++ src/TermPlayer.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <optional>
+#include "IPlayer.hpp"
+
+class TermPlayer : public IPlayer {
+public:
+    TermPlayer();
+    ~TermPlayer();
+    void                         set_win(char player) override;
+    void                         set_draw(void) override;
+    std::optional<unsigned int> &get_move(char player) override;
+    void                         set_player_symbol(char player) override;
+    void set_board_state(const std::array<char, 9> &board) override;
+    bool is_done() override;
+
+private:
+    std::optional<unsigned int> _move_made;
+};
